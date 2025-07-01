363c1b69370fc1c5684580e4152db2bdfff53c44
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-01 13:25:19 -0500

    feat(service): crea interfaz IRegionService para gestión de regiones

---
+++ src/main/java/com/guildgate/web/Service/IRegionService.java
@@ -0,0 +1,12 @@
+package com.guildgate.web.Service;
+
+import com.guildgate.web.Modelo.Region;
+import com.guildgate.web.Service.generic.ICrudService;
+
+/**
+ *
+ * @author Juan - Luis
+ */
+public interface IRegionService extends ICrudService<Region, Integer>{
+    
+}
