b55f90120a79007c0997ec1157ab077794cb18ae
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-01 13:23:41 -0500

    feat(service): agrega interfaz IBossesService extendiendo ICrudService

---
+++ src/main/java/com/guildgate/web/Service/IBossesService.java
@@ -0,0 +1,12 @@
+package com.guildgate.web.Service;
+
+import com.guildgate.web.Modelo.Bosses;
+import com.guildgate.web.Service.generic.ICrudService;
+
+/**
+ *
+ * @author Juan - Luis
+ */
+public interface IBossesService extends ICrudService<Bosses, Integer>{
+    
+}
