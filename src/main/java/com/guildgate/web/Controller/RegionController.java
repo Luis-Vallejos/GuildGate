7cea1f44052e79b2f850113ae9fb02dc6a29977e
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-04 17:44:57 -0500

    feat(controller): agregar métodos CRUD en RegionController

--- src/main/java/com/guildgate/web/Controller/RegionController.java
+++ src/main/java/com/guildgate/web/Controller/RegionController.java
@@ -1,9 +1,44 @@
 package com.guildgate.web.Controller;
 
+import com.guildgate.web.Modelo.Region;
+import com.guildgate.web.Service.RegionService;
+import jakarta.inject.Inject;
+import java.util.ArrayList;
+
 /**
  *
- * @author  Juan - Luis
+ * @author Juan - Luis
  */
 public class RegionController {
-    
+
+    @Inject
+    RegionService rs;
+
+    public RegionController() {
+        this.rs = new RegionService();
+    }
+
+    public boolean creacion(Region r) {
+        boolean estado = rs.create(r);
+        return estado;
+    }
+
+    public boolean edicion(Region r) {
+        boolean estado = rs.edit(r);
+        return estado;
+    }
+
+    public boolean eliminar(Integer id) {
+        boolean estado = rs.delete(id);
+        return estado;
+    }
+
+    public Region buscarPorId(Integer id) {
+        return rs.findById(id);
+    }
+
+    public ArrayList<Region> buscarTodos() {
+        return rs.findAll();
+    }
+
 }
