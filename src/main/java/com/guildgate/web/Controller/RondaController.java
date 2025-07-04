a919b93f5a86d9678ba74d218d6705bec3570731
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-04 17:45:27 -0500

    feat(controller): implementar operaciones CRUD en RondaController

--- src/main/java/com/guildgate/web/Controller/RondaController.java
+++ src/main/java/com/guildgate/web/Controller/RondaController.java
@@ -1,9 +1,43 @@
 package com.guildgate.web.Controller;
 
+import com.guildgate.web.Modelo.Ronda;
+import com.guildgate.web.Service.RondaService;
+import jakarta.inject.Inject;
+import java.util.ArrayList;
+
 /**
  *
  * @author Juan - Luis
  */
 public class RondaController {
-    
+
+    @Inject
+    RondaService rs;
+
+    public RondaController() {
+        this.rs = new RondaService();
+    }
+
+    public boolean creacion(Ronda r) {
+        boolean estado = rs.create(r);
+        return estado;
+    }
+
+    public boolean edicion(Ronda r) {
+        boolean estado = rs.edit(r);
+        return estado;
+    }
+
+    public boolean eliminar(Integer id) {
+        boolean estado = rs.delete(id);
+        return estado;
+    }
+
+    public Ronda buscarPorId(Integer id) {
+        return rs.findById(id);
+    }
+
+    public ArrayList<Ronda> buscarTodos() {
+        return rs.findAll();
+    }
 }
