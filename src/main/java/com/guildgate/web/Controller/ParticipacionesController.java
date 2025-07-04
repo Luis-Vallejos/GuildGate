b6d4face1b5d065109e0b41e8b78946b163e42bc
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-04 17:44:12 -0500

    feat(controller): añadir operaciones CRUD en ParticipacionesController

--- src/main/java/com/guildgate/web/Controller/ParticipacionesController.java
+++ src/main/java/com/guildgate/web/Controller/ParticipacionesController.java
@@ -1,9 +1,44 @@
 package com.guildgate.web.Controller;
 
+import com.guildgate.web.Modelo.Participaciones;
+import com.guildgate.web.Service.ParticipacionesService;
+import jakarta.inject.Inject;
+import java.util.ArrayList;
+
 /**
  *
  * @author Juan - Luis
  */
 public class ParticipacionesController {
-    
+
+    @Inject
+    ParticipacionesService ps;
+
+    public ParticipacionesController(ParticipacionesService ps) {
+        this.ps = new ParticipacionesService();
+    }
+
+    public boolean creacion(Participaciones p) {
+        boolean estado = ps.create(p);
+        return estado;
+    }
+
+    public boolean edicion(Participaciones p) {
+        boolean estado = ps.edit(p);
+        return estado;
+    }
+
+    public boolean eliminar(Integer id) {
+        boolean estado = ps.delete(id);
+        return estado;
+    }
+
+    public Participaciones buscarPorId(Integer id) {
+        return ps.findById(id);
+    }
+
+    public ArrayList<Participaciones> buscarTodo() {
+        return ps.findAll();
+    }
+
 }
