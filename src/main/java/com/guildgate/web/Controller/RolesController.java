f899dc0e8818df344fba6f61104bc04e1bb67232
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-04 17:45:12 -0500

    feat(controller): agregar operaciones CRUD en RolesController

--- src/main/java/com/guildgate/web/Controller/RolesController.java
+++ src/main/java/com/guildgate/web/Controller/RolesController.java
@@ -1,9 +1,44 @@
 package com.guildgate.web.Controller;
 
+import com.guildgate.web.Modelo.Roles;
+import com.guildgate.web.Service.RoleService;
+import jakarta.inject.Inject;
+import java.util.ArrayList;
+
 /**
  *
  * @author Juan - Luis
  */
 public class RolesController {
-    
+
+    @Inject
+    RoleService rs;
+
+    public RolesController() {
+        this.rs = new RoleService();
+    }
+
+    public boolean creacion(Roles r) {
+        boolean estado = rs.create(r);
+        return estado;
+    }
+
+    public boolean edicion(Roles r) {
+        boolean estado = rs.edit(r);
+        return estado;
+    }
+
+    public boolean eliminar(Integer id) {
+        boolean estado = rs.delete(id);
+        return estado;
+    }
+
+    public Roles buscarPorId(Integer id) {
+        return rs.findById(id);
+    }
+
+    public ArrayList<Roles> buscarTodos() {
+        return rs.findAll();
+    }
+
 }
