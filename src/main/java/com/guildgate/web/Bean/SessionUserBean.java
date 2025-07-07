5005ce189b1a76e0f84443d2c1dfca6a60aac949
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-07 14:02:56 -0500

    feat(bean): agregar SessionUserBean para encapsular datos de sesión del usuario

---
+++ src/main/java/com/guildgate/web/Bean/SessionUserBean.java
@@ -0,0 +1,27 @@
+package com.guildgate.web.Bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author Juan - Luis
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class SessionUserBean {
+
+    private UserProfileBean perfil;
+    private AvatarBean avatar;
+    private BannerBean banner;
+    private GuildBean gremio;
+}
