2a7992b9e780a992080f5f812c8fcf7884194f0d
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-07-10 14:21:21 -0500

    feat(service): agregar interfaz IRolPermisoService que extiende ICrudService

---
+++ src/main/java/com/guildgate/web/Service/IRolPermisoService.java
@@ -0,0 +1,13 @@
+package com.guildgate.web.Service;
+
+import com.guildgate.web.Modelo.RolPermiso;
+import com.guildgate.web.Modelo.RolPermisoId;
+import com.guildgate.web.Service.generic.ICrudService;
+
+/**
+ *
+ * @author Juan - Luis
+ */
+public interface IRolPermisoService extends ICrudService<RolPermiso, RolPermisoId>{
+    
+}
